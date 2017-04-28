a78aecc70285beafe7b343f36c47e249cd169ec7
Kacerja <EKAC@EUR.AD.SAG>
2017-04-29 02:21:57 +0300

    added read me

--- src/tech/aubg/demoproject/dao/BookDAOImp.java
+++ src/tech/aubg/demoproject/dao/BookDAOImp.java
@@ -2,7 +2,6 @@ package tech.aubg.demoproject.dao;
 
 import java.util.List;
 
-
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.query.Query;
@@ -12,10 +11,9 @@ import org.springframework.transaction.annotation.Transactional;
 
 import tech.aubg.demoproject.entities.Book;
 
-
 @Repository
 public class BookDAOImp implements BookDAO {
-	
+
 	@Autowired
 	SessionFactory sessionFactory;
 
@@ -24,50 +22,47 @@ public class BookDAOImp implements BookDAO {
 	public List<Book> getBooks() {
 
 		Session currentSession = sessionFactory.getCurrentSession();
-		
+
 		Query<Book> myQuery = currentSession.createQuery("from Book order by title", Book.class);
-		
+
 		List<Book> books = myQuery.getResultList();
-		
+
 		return books;
 	}
 
-	
 	@Override
 	@Transactional
 	public void saveBook(Book newBook) {
-		
-		//  get current session from hibernate
-		Session currentSession = sessionFactory.getCurrentSession();
-		
-		//  save the new book
-		currentSession.saveOrUpdate(newBook);
-		
-	}
 
+		// get current session from hibernate
+		Session currentSession = sessionFactory.getCurrentSession();
+
+		// save the new book
+		currentSession.saveOrUpdate(newBook);
+
+	}
 
 	@Override
 	@Transactional
 	public Book getBook(int bookId) {
 
 		Session currentSession = sessionFactory.getCurrentSession();
-		
+
 		Book book = currentSession.get(Book.class, bookId);
-		
+
 		return book;
 	}
-
 
 	@Override
 	@Transactional
 	public void deleteBook(int bookId) {
-		
+
 		Session currentSession = sessionFactory.getCurrentSession();
-		
-		Query theQuery = currentSession.createQuery("delete from Book where bookId="+bookId);
-		
+
+		Query theQuery = currentSession.createQuery("delete from Book where bookId=" + bookId);
+
 		theQuery.executeUpdate();
-		
+
 	}
 
 }
