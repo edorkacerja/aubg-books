0e6cd5a1a734e7fb626aa7e50921470dd4e42e15
Kacerja <EKAC@EUR.AD.SAG>
2017-05-06 22:38:39 +0300

    updated sql scripts

--- creation_script.sql
+++ creation_script.sql
@@ -12,8 +12,18 @@ CREATE TABLE `book` (
   `title` varchar(45) DEFAULT NULL,
   `author_id` int(11) DEFAULT NULL,
   `pages` int(11) DEFAULT NULL,
+  `book_printing_house_id` int(11) DEFAULT NULL,
   PRIMARY KEY (`book_id`),
   KEY `author_id_idx` (`author_id`),
-  CONSTRAINT `author_id` FOREIGN KEY (`author_id`) REFERENCES `author` (`author_id`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB AUTO_INCREMENT=18 DEFAULT CHARSET=utf8;
+  KEY `book_printing_house_id_idx` (`book_printing_house_id`),
+  CONSTRAINT `author_id` FOREIGN KEY (`author_id`) REFERENCES `author` (`author_id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `book_printing_house_id` FOREIGN KEY (`book_printing_house_id`) REFERENCES `book_printing_house` (`book_printing_house_id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
 
+
+CREATE TABLE `book_printing_house` (
+  `book_printing_house_id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(45) NOT NULL,
+  `address` varchar(45) NOT NULL,
+  PRIMARY KEY (`book_printing_house_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
--- data_input_script.sql
+++ data_input_script.sql
@@ -1,23 +1,181 @@
 INSERT INTO `test_db`.`author`
-(`author_id`,
-`name`,
+(`name`,
 `biography`,
 `nationality`)
 VALUES
-(<{author_id: }>,
-<{name: }>,
-<{biography: }>,
-<{nationality: }>);
+('Mark Twain',
+'sample biography',
+'american');
+
+
+
+INSERT INTO `test_db`.`author`
+(`name`,
+`biography`,
+`nationality`)
+VALUES
+('Jules Verne',
+'sample biography',
+'french');
+
+
+INSERT INTO `test_db`.`author`
+(`name`,
+`biography`,
+`nationality`)
+VALUES
+('Ismail Kadare',
+'sample biography',
+'albanian');
+
+
+INSERT INTO `test_db`.`author`
+(`name`,
+`biography`,
+`nationality`)
+VALUES
+('Dritero Agolli',
+'sample biography',
+'albanian');
+
+
+INSERT INTO `test_db`.`author`
+(`name`,
+`biography`,
+`nationality`)
+VALUES
+('Dan Brown',
+'sample biography',
+'american');
+
+INSERT INTO `test_db`.`author`
+(`name`,
+`biography`,
+`nationality`)
+VALUES
+('J K Rowling',
+'sample biography',
+'english');
+
+INSERT INTO `test_db`.`author`
+(`name`,
+`biography`,
+`nationality`)
+VALUES
+('Agatha Cristie',
+'sample biography',
+'american');
+
+
+
+
+INSERT INTO `test_db`.`book_printing_house`
+(`name`,
+`address`)
+VALUES
+('Best Publishing house',
+'albania');
+
+
+INSERT INTO `test_db`.`book_printing_house`
+(`name`,
+`address`)
+VALUES
+('Second Best Publishing house',
+'albania');
+
+
+INSERT INTO `test_db`.`book_printing_house`
+(`name`,
+`address`)
+VALUES
+('Third Best Publishing house',
+'albania');
+
+
+INSERT INTO `test_db`.`book_printing_house`
+(`name`,
+`address`)
+VALUES
+('Fourth Best Publishing house',
+'albania');
+
+INSERT INTO `test_db`.`book_printing_house`
+(`name`,
+`address`)
+VALUES
+('Fifth Best Publishing house',
+'albania');
+
 
 
 
 INSERT INTO `test_db`.`book`
-(`book_id`,
-`title`,
-`author`,
-`pages`)
+(`title`,
+`author_id`,
+`pages`,
+`book_printing_house_id`)
 VALUES
-(<{book_id: }>,
-<{title: }>,
-<{author: }>,
-<{pages: }>);
+('20 thousand leages under the water',
+2,
+300,
+1);
+
+
+INSERT INTO `test_db`.`book`
+(`title`,
+`author_id`,
+`pages`,
+`book_printing_house_id`)
+VALUES
+('20 thousand leages under the water',
+2,
+300,
+1);
+
+
+INSERT INTO `test_db`.`book`
+(`title`,
+`author_id`,
+`pages`,
+`book_printing_house_id`)
+VALUES
+('20 thousand leages under the water',
+2,
+300,
+1);
+
+
+INSERT INTO `test_db`.`book`
+(`title`,
+`author_id`,
+`pages`,
+`book_printing_house_id`)
+VALUES
+('book 3',
+3,
+300,
+2);
+
+INSERT INTO `test_db`.`book`
+(`title`,
+`author_id`,
+`pages`,
+`book_printing_house_id`)
+VALUES
+('book four',
+4,
+300,
+3);
+
+
+INSERT INTO `test_db`.`book`
+(`title`,
+`author_id`,
+`pages`,
+`book_printing_house_id`)
+VALUES
+('Harry potter',
+6,
+300,
+1);
